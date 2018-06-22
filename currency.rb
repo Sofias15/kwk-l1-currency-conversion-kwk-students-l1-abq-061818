c836bf43975b212a5a62f70eab2eb4016491206e
Sofias15 <anasofiasanch@gmail.com>
2018-06-22 16:04:50 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,18 @@
-# Write your cod here
+#if "us_d"
+ # == "86_euros"
+  #then " us_d / 86_euros"
+
+
+#"usd = $1.00"
+#" us d" % euros ="conversion"
+
+ puts "pick from the following menu:"
+ 
+ puts "1. usd_to_eur"
+ puts "2. eur_to_usd"
+ puts "How much money do you want to exchange"
+ 
+ amount= gets.amount( ),.to_i
+ 
+
+ 
